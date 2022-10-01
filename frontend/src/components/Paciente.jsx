3fb8e5840b6d5f300044881960d45709211730c8
Jorge Gárate <jorgegarate@MacBook-Pro-de-SII.local>
2022-10-01 01:23:42 -0300

    carga de estilo pacientes

--- frontend/src/components/Paciente.jsx
+++ frontend/src/components/Paciente.jsx
@@ -1,6 +1,44 @@
-const Paciente = (paciente) => {
+const Paciente = ({paciente}) => {
+    const {email, nombre, rut, contacto, emailcontacto, fecha, sintomas, _id} =paciente
     return (
-        <h1>desde pacietne</h1>
+        <div className="mx-5 my-10 bg-white shadow-sm px-5 py-10 rounded-xl">
+            <p className="font-bold uppercase">Nombre: {' '}
+                <span className="font-normal normal-case">
+                    {nombre}
+                </span>
+            </p>
+            <p className="font-bold uppercase">Rut: {' '}
+                <span className="font-normal normal-case">
+                    {rut}
+                </span>
+            </p>
+            <p className="font-bold uppercase">Fecha: {' '} 
+                <span className="font-normal normal-case">
+                    {fecha}
+                </span>
+            </p>
+            <p className="font-bold uppercase">Email: {' '}
+                <span className="font-normal normal-case">
+                    {email}
+                </span>
+            </p>
+            <p className="font-bold uppercase">Sintomas: {' '}
+                <span className="font-normal normal-case">
+                    {sintomas}
+                </span>
+            </p>
+            <p className="font-bold uppercase">Contacto Emergencia: {' '} 
+                <span className="font-normal normal-case">
+                    {contacto}
+                </span>
+            </p>
+            <p className="font-bold uppercase">Email Emergencia: {' '}
+                <span className="font-normal normal-case">
+                    {emailcontacto}
+                </span>
+            </p>
+            
+        </div>
     );
 };
 export default Paciente;
